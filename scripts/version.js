ad3d1a732e898ecc99d84f2cd1800823530d4d77
jibe77 <jb.renaux@me.com>
2021-04-05 23:59:45 +0200

    add --localize

--- scripts/version.js
+++ scripts/version.js
@@ -4,12 +4,16 @@ const fs = require('fs');
 const path = require('path');
 const pjPath = path.resolve(path.dirname(__filename), '../package.json');
 const versionPath = path.resolve(path.dirname(__filename), '../version');
-const distVersionPath = path.resolve(path.dirname(__filename), '../dist/hermanas-client/assets/version');
+const distVersionPathEn = path.resolve(path.dirname(__filename), '../dist/hermanas-client/en/assets/version');
+const distVersionPathEnUs = path.resolve(path.dirname(__filename), '../dist/hermanas-client/en-US/assets/version');
+const distVersionPathFr = path.resolve(path.dirname(__filename), '../dist/hermanas-client/fr/assets/version');
 
 const pj = require(pjPath);
 
 console.log(`### INFO: Current Version: ${pj.version}`);
 fs.writeFileSync(versionPath, pj.version);
-fs.writeFileSync(distVersionPath, pj.version);
+fs.writeFileSync(distVersionPathEn, pj.version);
+fs.writeFileSync(distVersionPathEnUs, pj.version);
+fs.writeFileSync(distVersionPathFr, pj.version);
 
 module.exports = pj.version;

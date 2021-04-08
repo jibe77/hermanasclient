a3f97a5ab4c54afa670efd6938bf54fcbcb963a8
jibe77 <jb.renaux@me.com>
2021-04-08 18:02:13 +0200

    i18n

--- scripts/index.js
+++ scripts/index.js
@@ -8,3 +8,24 @@ const indexFile = path.resolve(path.dirname(__filename), '../dist/hermanas-clien
 fs.copyFile(redirectFile, indexFile,  () => {
     console.log(`### INFO: File copied from : ${redirectFile} to ${indexFile}`);
 });
+
+let srcFaviconFile = path.resolve(path.dirname(__filename), '../src/favicon.ico');
+let destFaviconFile = path.resolve(path.dirname(__filename), '../dist/hermanas-client/favicon.ico');
+
+fs.copyFile(srcFaviconFile, destFaviconFile,  () => {
+    console.log(`### INFO: File copied from : ${srcFaviconFile} to ${destFaviconFile}`);
+});
+
+srcFaviconFile = path.resolve(path.dirname(__filename), '../src/favicon144.png');
+destFaviconFile = path.resolve(path.dirname(__filename), '../dist/hermanas-client/favicon144.png');
+
+fs.copyFile(srcFaviconFile, destFaviconFile,  () => {
+    console.log(`### INFO: File copied from : ${srcFaviconFile} to ${destFaviconFile}`);
+});
+
+srcFaviconFile = path.resolve(path.dirname(__filename), '../src/favicon512.png');
+destFaviconFile = path.resolve(path.dirname(__filename), '../dist/hermanas-client/favicon512.png');
+
+fs.copyFile(srcFaviconFile, destFaviconFile,  () => {
+    console.log(`### INFO: File copied from : ${srcFaviconFile} to ${destFaviconFile}`);
+});

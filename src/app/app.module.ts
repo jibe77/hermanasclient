91a3b013e22987f0ba2e3bb8d4c619fc17b35708
jibe77 <jb.renaux@icloud.com>
2021-11-12 14:43:51 +0100

    update angular modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RxStompService } from '@stomp/ng2-stompjs';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        NgbModule,
+    ],
     providers: [RxStompService],
     bootstrap: [AppComponent],
 })
